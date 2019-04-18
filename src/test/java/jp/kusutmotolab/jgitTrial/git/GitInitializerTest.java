3d972eb3829528cf4aeb6d791849de95f0e49a61
tsk-nkgw <tasktas9@gmail.com>
2019-04-18 19:14:15 +0900

    add test

--- src/test/java/jp/kusutmotolab/jgitTrial/git/GitInitializerTest.java
+++ src/test/java/jp/kusutmotolab/jgitTrial/git/GitInitializerTest.java
@@ -1,8 +1,19 @@
 package jp.kusutmotolab.jgitTrial.git;
 
-import static org.junit.Assert.*;
+import org.eclipse.jgit.api.Git;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GitInitializerTest {
 
+    @Test
+    public void testInitializer() throws IOException {
+        final GitInitializer gitInitializer = new GitInitializer(Paths.get("sample"));
+        assertThat(gitInitializer.initialize()).isNotNull().isInstanceOf(Git.class);
+    }
 
 }
