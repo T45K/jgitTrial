b19d67922fdd69ad1a59fbf208a380baa68f5b6b
tsk-nkgw <tasktas9@gmail.com>
2019-04-18 18:52:19 +0900

    example usage

--- src/main/java/jp/kusutmotolab/jgitTrial/Main.java
+++ src/main/java/jp/kusutmotolab/jgitTrial/Main.java
@@ -1,7 +1,12 @@
 package jp.kusutmotolab.jgitTrial;
 
+import jp.kusutmotolab.jgitTrial.git.GitInitializer;
+import org.eclipse.jgit.api.Git;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws IOException {
+        final Git git = new GitInitializer(args[0]).initilize();
     }
 }
