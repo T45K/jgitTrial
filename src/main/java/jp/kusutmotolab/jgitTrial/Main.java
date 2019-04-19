78a714b5f3b56fe4c6dd82c8291c29992970494e
tsk-nkgw <tasktas9@gmail.com>
2019-04-19 14:33:20 +0900

    add throw

--- src/main/java/jp/kusutmotolab/jgitTrial/Main.java
+++ src/main/java/jp/kusutmotolab/jgitTrial/Main.java
@@ -4,6 +4,8 @@ import jp.kusutmotolab.jgitTrial.git.GitSetUpper;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
+import java.io.IOException;
+
 public class Main {
     private Configuration configuration;
 
@@ -11,13 +13,13 @@ public class Main {
         this.configuration = configuration;
     }
 
-    public static void main(String[] args) throws GitAPIException {
+    public static void main(String[] args) throws GitAPIException, IOException {
         final Configuration configuration = Configuration.Builder.buildFromArgs(args);
         final Main main = new Main(configuration);
         main.run();
     }
 
-    private void run() throws GitAPIException {
+    private void run() throws GitAPIException, IOException {
         final GitSetUpper gitSetUpper = new GitSetUpper(configuration.getLocalPath(), configuration.getRemoteURI());
         final Git git = gitSetUpper.setUp();
     }
